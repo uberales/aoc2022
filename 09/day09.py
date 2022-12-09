8f5fe3614e80241187da91ac0c342a9ac0a1a5d7
Aleš Podolník <ales@podolnik.me>
2022-12-09 15:56:44 +0100

    Day 9, visualisation

--- 09/day09.py
+++ 09/day09.py
@@ -35,23 +35,23 @@ def follow(head, tail):
 head = (0, 0)
 tail = (0, 0)
 
-tail_history = set()
-tail_history.add(tail)
+tail_history_1 = set()
+tail_history_1.add(tail)
 
 for move in data:
     for _ in range(move[1]):
         d = directions[move[0]]
         head = (head[0] + d[0], head[1] + d[1])
         tail = follow(head, tail)
-        tail_history.add(tail)
+        tail_history_1.add(tail)
         
-print(len(tail_history))
+print(len(tail_history_1))
 
 # part 2
 rope = [(0, 0)] * 10
 
-tail_history = set()
-tail_history.add(rope[-1])
+tail_history_2 = set()
+tail_history_2.add(rope[-1])
 
 for move in data:
     for _ in range(move[1]):
@@ -59,22 +59,28 @@ for move in data:
         rope[0] = (rope[0][0] + d[0], rope[0][1] + d[1])        
         for i in range(1, len(rope)):
             rope[i] = follow(rope[i - 1], rope[i])
-        tail_history.add(rope[-1])
+        tail_history_2.add(rope[-1])
             
-print(len(tail_history))
+print(len(tail_history_2))
 
 # visualisation
 
-tail = sorted(tail_history)
-tail_r = [t[0] for t in tail]
-tail_c = [t[1] for t in tail]
+tail1 = sorted(tail_history_1)
+tail1_r = [t[0] for t in tail1]
+tail1_c = [t[1] for t in tail1]
+
+tail2 = sorted(tail_history_2)
+tail2_r = [t[0] for t in tail2]
+tail2_c = [t[1] for t in tail2]
 
 fig, ax = plt.subplots()
-ax.scatter(tail_c, tail_r, s=1)
+ax.scatter(tail1_c, tail1_r, s=1, label='#1', alpha = 0.5)
+ax.scatter(tail2_c, tail2_r, s=1, label='#2', alpha = 0.5)
 ax.set_xlabel('column')
 ax.set_ylabel('row')
 ax.set_aspect('equal')
 ax.set_title('Tail location')
+ax.legend()
 plt.savefig('tail.png', bbox_inches='tight')
 plt.show()
 plt.close()
